499fc9c83f05f7d3d2d306724bbdc6c956e9d973
Christoffer Hald Christensen <chrishald@proton.me>
2025-12-09 10:34:56 +0100

    Added SDL visualization

--- flake.nix
+++ flake.nix
@@ -10,12 +10,14 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib;
     in
     {
       formatter.${system} = pkgs.nixfmt-rfc-style;
 
-      devShells.${system}.default = pkgs.mkShell.override { inherit (pkgs.llvmPackages_latest) stdenv; } {
+      devShells.${system}.default = pkgs.mkShell.override { inherit (pkgs.llvmPackages_latest) stdenv; } rec {
         packages = with pkgs; [
+          zig
           cmake
           ninja
           llvmPackages_latest.lld
@@ -23,6 +25,7 @@
             p.customtkinter
             p.tkinter
           ]))
+          llvmPackages_latest.lldb
 
           wayland
           wayland-scanner
@@ -35,7 +38,16 @@
           vulkan-headers
           vulkan-loader
           libdecor
+
+          (lib.getDev sdl3)
+          (lib.getDev sdl3-ttf)
+          (lib.getDev sdl3-image)
         ];
-      };
+
+        NIX_LDFLAGS =
+          "-rpath ${
+            lib.makeLibraryPath packages
+          }";
+        };
     };
 }
--- src/display.h
+++ src/display.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include "SDL3/SDL.h"
 #include "cell.h"
 
-void display(const CellularAutomaton* automaton);
+typedef struct SDLState {
+    SDL_Window* win;
+    SDL_Surface* surf;
+    int w;
+    int h;
+} SDLState;
+
+SDLState initSDL(int w, int h);
+
+void display(const SDLState* surface, const CellularAutomaton* automaton);
