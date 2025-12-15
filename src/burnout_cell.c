00e6e82509f1f45a1331ff80daefb70af36b23f7
krak <mkrak25@student.aau.dk>
2025-12-15 13:08:35 +0100

    implemented burnoutcell function

--- src/burnout_cell.c
+++ src/burnout_cell.c
@@ -1,8 +1,54 @@
 #include "burnout_cell.h"
-#include "cell.h"
+
+static const size_t burn_durations[VEG_LAST] = {
+    1, // Broadleaves - 2.55min + 40sec - 1 timestep (rounded down)
+    2, // Shrubs - 2.55min + 45sec - 1 timestep (rounded down)
+    3, // Grassland 2.55min + 15-30sec (averaging to 25sec) - 1 timestep (rounded down)
+    4, // Fireprone (pines) - 2.55min + 167sec - 2 timesteps (rounded down)
+    5, // Agroforestry (trees (spruce) and shrubs) - 2.55min + 65min - 4.053 sec - 26 timesteps (rounded down)
+    6, // Notfireprone (trees (spruce)) 2 timer (twice as much time, so 52 timesteps) (we may have to tweak these numbers)
+};
+
+
+// The function that checks if a cell is burned out
+static bool isBurnedOut(const Cell *cell)
+{
+    // If the cell is not burning, we can check if it is bruned out.
+    // It is possible that the cell can be unburned or already burned out
+    if (cell->state != CELLSTATE_ONFIRE) {
+        return false; // returns false, because we only check active burning cells
+    }
+
+    // Check if the time it burns exceeds the time for the given vegetation, to see if the cell is supposed to be "burned out"
+    return cell->on_fire_counter >= burn_durations[vegTypeIndex(cell->type)];
+
+}
 
 CellularAutomaton burnoutCells(const CellularAutomaton* automaton) {
-    CellularAutomaton result = cloneAutomaton(automaton);
+    CellularAutomaton res = cloneAutomaton(automaton);
 
-    return result;
+    // Looping through all the rows
+    for (size_t row = 0; row < res.num_rows; row++) {
+        CellArray arr = res.rows[row];
+
+            //Loop through all the collums in the rows
+        for (size_t col = 0; col < arr.count; col++) {
+            printf("row: %zu, col: %zu\n", row, col);
+
+            // get actual cell insted of the cloned version
+            Cell* cell = &arr.elements[col];
+
+            // Check if the cell is burned out
+            if (isBurnedOut(cell))
+            {
+                cell->state = CELLSTATE_BURNT;
+
+              // If the cell is burning apply the counter
+            } else if (cell->state == CELLSTATE_ONFIRE) {
+                cell-> on_fire_counter++;
+            }
+        }
+    }
+
+ return res;
 }
